ee94b2d847507326638c505f2f61a558cbfe261e
metalhead224 <107532348+metalhead224@users.noreply.github.com>
2022-06-26 20:33:50 +0545

    Add files via upload
    i added, basic drum kit sounds and buttons.

---
+++ index.js
@@ -0,0 +1,69 @@
+
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (i=0; i<numberOfDrums; i++ ){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+    var buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHtml);
+
+  });
+}
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+    break;
+
+    case "a":
+        var kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+    break;
+
+    case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+    break;
+
+    case "d":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+    break
+
+    case "j":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+    break;
+
+    case "k":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+    break;
+
+    case "l":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+    break;
+
+    case "q":
+        var openHihat = new Audio("sounds/open-hihat.mp3");
+        openHihat.play();
+    break;
+
+    case "e":
+        var closedHihat = new Audio("sounds/closed-hihat.mp3");
+        closedHihat.play();
+    break;
+
+
+    default:
+
+  }
+}
